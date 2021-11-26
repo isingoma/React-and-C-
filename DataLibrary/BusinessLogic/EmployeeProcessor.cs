e7eb81fd1cf0d68827b327a29de6a334dd23e636
isingoma <40125380+isingoma@users.noreply.github.com>
2021-11-26 10:02:51 +0300

    Add files via upload

---
+++ DataLibrary/BusinessLogic/EmployeeProcessor.cs
@@ -0,0 +1,40 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+	public static class EmployeeProcessor
+	{
+		public static int CreateEmployee(int employeeId, string firstName,
+			string lastName, string emailAddress)
+		{
+			EmployeeModel data = new EmployeeModel
+			{
+				
+				EmployeeID = employeeId,
+				FirstName = firstName,
+				LastName = lastName,
+				Email = emailAddress
+			};
+
+			string sql = @"insert into dbo.Employee (EmployeeID, FirstName, LastName, Email)
+                           values (@EmployeeID, @FirstName, @LastName, @Email);";
+
+			return SqlDataAccess.SaveData(sql, data);
+		}
+
+		public static List<EmployeeModel> LoadEmployees()
+		{
+			string sql = @"select Id, EmployeeID, FirstName, LastName, Email
+                           from dbo.Employee;";
+
+			return SqlDataAccess.LoadData<EmployeeModel>(sql);
+		}
+	}
+}
+
